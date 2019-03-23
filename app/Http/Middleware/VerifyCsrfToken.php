582bd9f3b6f199369f81321befe75a7ef787acff
bwp980220 <1596743757@qq.com>
2019-03-23 13:54:55 +0800

    add

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -20,9 +20,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        'alipay/*',
-        'http://login/do',
-        'http://register/do',
-        'http://code',
+        "*"
     ];
 }
